d39db92a9b52e982dde97bfc7582b1734d580b47
amirghaderian <it.ghaderian>
2023-01-15 00:51:01 +0330

    fix form bug

---
+++ movies-form/src/components/moviesTable.jsx
@@ -0,0 +1,48 @@
+import Like from "./common/like";
+import { Component } from "react";
+import Table from "./common/table";
+import { Link } from "react-router-dom";
+class MoviesTable extends Component {
+  columns = [
+    {
+      path: "title",
+      lable: "Title",
+      content: (movie) => (
+        <Link to={`/movies/${movie._id}`}> {movie.title}</Link>
+      ),
+    },
+    { path: "genre.name", lable: "Genre" },
+    { path: "numberInStock", lable: "Stock" },
+    { path: "dailyRentalRate", lable: "Rate" },
+    {
+      key: "like",
+      content: (movie) => (
+        <Like liked={movie.liked} onLike={() => this.props.onLike(movie)} />
+      ),
+    },
+    {
+      key: "delete",
+      content: (movie) => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className="btn btn-danger btn-danger"
+        >
+          Delete
+        </button>
+      ),
+    },
+  ];
+  render() {
+    debugger;
+    const { movies, onSort, sortColumn } = this.props;
+    return (
+      <Table
+        columns={this.columns}
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
+    );
+  }
+}
+export default MoviesTable;
---
+++ movies-form/src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
+import { BrowserRouter } from "react-router-dom";
+import registerServiceWorker from "./registerServiceWorker";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+registerServiceWorker();
