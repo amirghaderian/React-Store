81d3bb1ba22667a9716b17e8a6bf6d7f78d9cb45
asd <asd.it@gmail.com>
2023-04-11 19:37:46 +0430

    Update Home.jsx

--- SORINAPP/src/components/Home.jsx
+++ SORINAPP/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
 import axios from "axios";
 const Home = () => {
