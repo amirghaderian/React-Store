a6f87c00031fd5314986df768997363c942e1923
asd <asd.it@gmail.com>
2023-04-15 12:17:33 +0430

    Update Comment.jsx

--- http-request/src/components/comment/Comment.jsx
+++ http-request/src/components/comment/Comment.jsx
@@ -3,16 +3,24 @@ import { useEffect, useState } from "react";
 import "../../assets/styles/comment.css";
 
 const Comment = () => {
-  const [person,setPerson]=(null)
-  const fetchData =()=> axios
-    .get("https://jsonplaceholder.typicode.com/comments").then((response)=>{setPerson(response.data);})
-    .catch((error)=>{console.log(error)});
-    useEffect(()=>{fetchData()},[])
+  const [person, setPerson] = useState(null);
+  const fetchData = () =>
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        setPerson(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    
     <div className="comment">
-       
       <div>
+        {person === null ? <div> noting to show</div> : person.map((p)=>{return <div><p>{p.name}</p>
+        <p></p></div>})}
         <p>name1</p>
         <p>email1</p>
       </div>
